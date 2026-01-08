d1dadea5be565a440038513c136898483f86e6bf
Sam Esfahani <ssesfahani@gmail.com>
2026-01-07 21:03:36 -0500

    ok

--- facebed.py
+++ facebed.py
@@ -339,11 +339,21 @@ class JsonParser:
         headers = {
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/jxl,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'en-US,en;q=0.9',
+            'accept-encoding': 'gzip, deflate, br, zstd',
             'cache-control': 'no-cache',
             'pragma': 'no-cache',
             'priority': 'u=0, i',
             'sec-fetch-mode': 'navigate',
             'sec-fetch-site': 'none',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-user': '?1',
+            'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+            'dnt': '1',
+            'connection': 'keep-alive',
         }
 
         return headers
